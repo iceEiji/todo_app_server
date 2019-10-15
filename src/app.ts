d09438e34f8322fda8dcea1125bb4eea43193edd
iceEiji <guitar.pg.sc+github@gmail.com>
2019-10-15 12:47:04 +0900

    session is never used.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, NextFunction } from 'express';
-import session from 'express-session'
 import createError from 'http-errors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
