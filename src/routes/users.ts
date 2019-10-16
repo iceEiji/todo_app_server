65304fbf3e326beb217e5cdf7554a330cf8e4bb0
iceEiji <guitar.pg.sc+github@gmail.com>
2019-10-16 13:02:34 +0900

    fix: tslint error was fixed by prettier formatter.
    * ERROR: src/routes/index.ts:5:12 - Replace `'/'` with `"/"`
    * ERROR: src/routes/index.ts:6:14 - Replace `'index',·{·title:·'Express·-·top'` with `"index",·{·title:·"Express·-·top"`
    * ERROR: src/routes/index.ts:9:9 - Replace `router·as·index` with `·router·as·index·`

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,9 +2,9 @@ import express, { Request, Response, NextFunction, Router } from "express";
 const router: Router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.render('users', { title: 'Express - users' });
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.render("users", { title: "Express - users" });
   // res.send('respond with a resource');
 });
 
-export {router as users};
+export { router as users };
