68b832aa53317024c9c97ecfd9cad626fc09fda8
iceEiji <guitar.pg.sc@gmail.com>
2021-05-11 00:54:13 +0900

    [ktor] DB周りの初期設定追加。

--- backend/ktor/src/Application.kt
+++ backend/ktor/src/Application.kt
@@ -1,5 +1,6 @@
 package com.todo.example
 
+import com.todo.example.factory.DatabaseFactory
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
@@ -11,6 +12,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    DatabaseFactory.init()
     routing {
         get("/") {
             call.respondText("Hello World!", ContentType.Text.Plain)
